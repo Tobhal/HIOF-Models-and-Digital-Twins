08b7d280fecc10d4f1c60c6d391182adc2d73c69
GjermundOstensvig <gjermund1995@gmail.com>
2023-02-08 21:26:12 -0800

    Added logic for setting temp during night. Also stopped tracking .java and .class files

--- Oblig1/TheRoomX3D2.MQTT.sim/thingml-gen/java/X3D_MQTT_SIM/src/main/java/org/thingml/generated/api/IEnergySaver_get_cmd.java
+++
@@ -1,8 +0,0 @@
-package org.thingml.generated.api;
-
-import org.thingml.generated.api.*;
-
-public interface IEnergySaver_get_cmd{
-void temperature_via_get_cmd(int TemperatureMsg_temperature_id_var, String TemperatureMsg_temperature_txt_var, double TemperatureMsg_temperature_t_var);
-void luminance_via_get_cmd(int LuminanceMsg_luminance_id_var, double LuminanceMsg_luminance_lum_var);
-}
