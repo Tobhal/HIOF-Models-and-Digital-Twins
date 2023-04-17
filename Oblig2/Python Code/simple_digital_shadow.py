38459d3d22ca9a89b50c168f9a465cc1dad1e85e
GjermundOstensvig <gjermund1995@gmail.com>
2023-04-16 22:23:41 -0700

    Shadow detects SwitchOff and SwitchOn

--- Oblig2/Python Code/simple_digital_shadow.py
+++ Oblig2/Python Code/simple_digital_shadow.py
@@ -5,14 +5,15 @@ broker="127.0.0.1"
 port=1883
 
 current_temperature = 20.0
-
+time_multiplier = 1 # Change this value to speed up or slow down the shadow.
+coming_values_queue = [20.0 for x in range(5)]
+switch_on = True
 
 def on_connect(client, userdata, flags, rc):
     if rc == 0:
         print("Connected to MQTT Broker!")
     else:
         print("Failed to connect, return code %d\n", rc)
-
 
 def publish_temperature(pub_temp: float, client):
     # Temperature message
@@ -28,9 +29,21 @@ def publish_temperature(pub_temp: float, client):
     ret = client.publish( topic="CPS2021/tempoutput", payload=json_object)
     # print(f"Broker returned: {ret}")
 
+def get_new_temp():
+    return 19.0
+
 def on_message(client, userdata, msg):
-    print(f"Received `{msg.payload.decode()}` from `{msg.topic}` topic")
-    publish_temperature(pub_temp=current_temperature, client=client)    
+    # print(f"Received `{msg.payload.decode()}` from `{msg.topic}` topic")
+    payload = msg.payload.decode() # e.g. {"SwitchOff":{"did":1}}
+    # deserializing the payload
+    mgs_data_dict = json.loads(payload)
+    # Check for whitch off mgs
+    if "SwitchOff" in mgs_data_dict.keys():
+        switch_on = False
+    elif "SwitchOn" in mgs_data_dict.keys():
+        switch_on = True
+    new_temp = get_new_temp()
+    publish_temperature(pub_temp=new_temp, client=client)
 
 my_client= paho.Client()                           #create client object
 my_client.on_connect = on_connect
