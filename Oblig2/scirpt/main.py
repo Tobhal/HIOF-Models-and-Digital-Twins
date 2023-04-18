beadb046f9e570c0735bb393f6314e4ddccf8fa4
Tobias Hallingstad <tobias@hallingstad.me>
2023-04-18 11:22:18 +0200

    Changes

--- Oblig2/scirpt/main.py
+++ Oblig2/scirpt/main.py
@@ -1,12 +1,26 @@
+from types import FunctionType
+from typing import Union
+
 import keras
 import paho.mqtt.client as mqtt
 import paho.mqtt.publish as publish
 from sklearn.preprocessing import MinMaxScaler
 from tensorflow.python.ops.numpy_ops import np_config
+
 np_config.enable_numpy_behavior()
 
 import numpy as np
 import matplotlib.pyplot as plt
+
+import time
+import threading
+
+sleep_time = 1
+
+last_temp = 16.0
+switch_on = 0
+last_hour = 0.0
+last_minute = 15
 
 
 def on_connect(client, userdata, flags, rc):
@@ -18,14 +32,24 @@ def on_connect(client, userdata, flags, rc):
 
 
 def on_message(client, userdata, msg):
-    print(msg.topic + " " + str(msg.payload))
+    if msg.payload in [b'{"SwitchOff":{"did":1}}', b'{"SwitchOn":{"did":1}}']:
+        print(str(msg.payload))
+    else:
+        print(msg.topic + " " + str(msg.payload))
 
 
-def main():
-    tmp = np.array([[19.6], [19.7], [19.8], [19.9]])
-    switch = np.array([[1], [1], [1], [1]])
-    hour = np.array([[10], [10.], [10.], [10.]])
-    min = np.array([[3], [6], [9], [12]])
+def send_temp(topic: str, model: Union[None, int, float, bool, list, dict, FunctionType]):
+    global last_temp, switch_on, last_hour, last_minute
+
+    tmp_arr = [[last_temp]]
+    switch_arr = [[switch_on]]
+    hour_arr = [[last_hour]]
+    min_arr = [[last_minute]]
+
+    tmp = np.array(tmp_arr)
+    switch = np.array(switch_arr)
+    hour = np.array(hour_arr)
+    min = np.array(min_arr)
 
     scaler = MinMaxScaler()
     scaler.fit(tmp)
@@ -38,12 +62,45 @@ def main():
     tmp_sc, switch_sc, hour_sc, min_sc = np.array(tmp_sc), np.array(switch_sc), np.array(hour_sc), np.array(min_sc)
     test = np.stack([tmp_sc, switch_sc, hour_sc, min_sc], axis=2)
 
-    model = keras.models.load_model('model')
-
     test_pred = model.predict(test)
     test_pred_inverse = scaler.inverse_transform(test_pred)
 
-    print(test_pred_inverse)
+    last_temp = test_pred_inverse[0][0]
+
+    print(round(test_pred_inverse[0][0], 1))
+    # print(last_temp, switch_on, last_hour, last_minute)
+
+    try:
+        publish.single(
+            'CPS2021/tempoutput',
+            payload=f'{{"temperature":{{"id":1,"txt":"temp","t":{round(test_pred_inverse, 1)}}}',
+            hostname='openhab-g2'
+        )
+    except:
+        pass
+
+    # payload = f'{{"temperature":{{"id":1,"txt":"temp","t":{str(round(test_pred_inverse[0][0], 0))}}}}}'
+
+    # publish.single(topic, payload=payload, hostname='openhab-g2')
+
+
+def run_thread(topic: str, model: Union[None, int, float, bool, list, dict, FunctionType]):
+    global last_minute, last_hour
+    while True:
+        send_temp(topic, model)
+        time.sleep(sleep_time)
+
+        last_minute += 3
+        if last_minute >= 60:
+            last_hour += 1
+            last_minute -= 60
+
+
+def main():
+    model = keras.models.load_model('model')
+
+    thread = threading.Thread(target=run_thread, args=('CPS2021/tempoutput', model))
+    thread.start()
 
     # pred = model.predict([[[10, 9, 8, 7]]])
     # print(pred)
@@ -54,11 +111,7 @@ def main():
 
     client.connect('openhab-g2', 1883, 60)
 
-    # publish a message to the MQTT network
-    publish.single('CPS2021/tempoutput', payload='{"temperature":{"id":1,"txt":"temp","t":30}}', hostname='openhab-g2')
-
     client.loop_forever()
-    pass
 
 
 if __name__ == '__main__':
